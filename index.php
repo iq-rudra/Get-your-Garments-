1ceb6735675f26e4adc4d3f2bcb9bfd54f59fd61
Siam183 <siam.qureshi@northsouth.edu>
2024-10-22 23:16:29 +0600

    Removed all recently added files

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1>Welcome to Wholesale Wardrobe</h1>";
-echo "<p><a href='admin_dashboard.php'>Go to Admin Dashboard</a></p>";
-echo "<p><a href='product_page.php'>View Products</a></p>";
-?>
